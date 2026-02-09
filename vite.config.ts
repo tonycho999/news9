7ece4ce030ef35019abba78742904bb5eb702f69
tonycho999 <83383706+tonycho999@users.noreply.github.com>
2026-02-09 14:15:50 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/news-api': {
+        target: 'https://gnews.io/api/v4/search',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/news-api/, '')
+      }
+    }
+  }
 })
